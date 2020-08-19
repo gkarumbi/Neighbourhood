03ce355b7b968861614aa6c898ef32f55ba138a7
kangalah <nyongesajoannanjala@gmail.com>
2020-08-19 06:52:27 +0300

    create the homepage

--- jirani/views.py
+++ jirani/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
