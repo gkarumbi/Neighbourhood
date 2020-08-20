98926cfdb50247092c18f3f6d004ae0926194825
Maratah <maratah7@gmail.com>
2020-08-20 20:02:42 +0300

    create files. -add inputs to the files. -solve bugs.

--- jirani/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Profile,Neighbourhood,Post,Business
-
-admin.site.register(Profile)
-admin.site.register(Neighbourhood)
-admin.site.register(Post)
-admin.site.register(Business)
--- jirani/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class JiraniConfig(AppConfig):
-    name = 'jirani'
