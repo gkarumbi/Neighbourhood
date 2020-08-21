38b8d077b275b7d75f438ec7312c94a25172ac73
kangalah <nyongesajoannanjala@gmail.com>
2020-08-21 06:51:31 +0300

    change nav bg-color

--- ghood/models.py
+++ ghood/models.py
@@ -5,10 +5,10 @@ from django.db.models.signals import post_save
 import datetime as dt
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(max_length=80, blank=True)
     bio = models.TextField(max_length=254, blank=True)
-    profile_picture = models.ImageField(upload_to='images/')
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
     location = models.CharField(max_length=50, blank=True, null=True)
     neighbourhood = models.ForeignKey('Neighbourhood', on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
 
